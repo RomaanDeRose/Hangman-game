4979f46589f0bee2ccb4f1efe822fafab3c00ec9
Roman De Rose <romandr37@gmail.com>
2025-02-09 22:29:53 -0300

    Elección de tema

--- script.js
+++ script.js
@@ -1,9 +1,146 @@
+// ELECION DE TEMAS
+const COLORS = [
+  {
+    color: "rojo",
+    valuePrimary: "#e84545",
+    valueSecondary: "#ea5455",
+    shadow1: "rgba(234, 84, 85, 0.3)",
+    shadow2: "rgba(234, 84, 85, 0.15)",
+  },
+  {
+    color: "azul",
+    valuePrimary: "#4553e8",
+    valueSecondary: "#5463ea",
+    shadow1: "rgba(84, 89, 234, 0.3)",
+    shadow2: "rgba(84, 89, 234, 0.15)",
+  },
+  {
+    color: "celeste",
+    valuePrimary: "#45e8e8",
+    valueSecondary: "#54eaea",
+    shadow1: "rgba(84, 234, 234, 0.3)",
+    shadow2: "rgba(84, 234, 234, 0.15)",
+  },
+  {
+    color: "rosa",
+    valuePrimary: "#c20d9b",
+    valueSecondary: "#ff22de",
+    shadow1: "rgba(255, 34, 222, 0.3)",
+    shadow2: "rgba(255, 34, 222, 0.15)",
+  },
+  {
+    color: "violeta",
+    valuePrimary: "#7b1fc2",
+    valueSecondary: "#a22ff1",
+    shadow1: "rgba(162, 47, 241, 0.3)",
+    shadow2: "rgba(162, 47, 241, 0.15)",
+  },
+  {
+    color: "naranja",
+    valuePrimary: "#ff8913",
+    valueSecondary: "#ff9f22",
+    shadow1: "rgba(255, 159, 34, 0.3)",
+    shadow2: "rgba(255, 159, 34, 0.15)",
+  },
+];
+
+const root = document.querySelector(":root");
+
+const setColors = (color) => {
+  const selectedColor = COLORS.find(
+    (c) => c.color.toUpperCase() == color.toUpperCase()
+  );
+  root.style.setProperty("--accent", selectedColor.valuePrimary);
+  root.style.setProperty("--secondary", selectedColor.valueSecondary);
+  root.style.setProperty("--shadowColor", selectedColor.shadow1);
+  root.style.setProperty("--shadowColor2", selectedColor.shadow2);
+};
+setColors(COLORS[0].color);
+
+const themesContainer = document.querySelector(".theme-options");
+const themesPicks = [];
+COLORS.forEach((color) => {
+  const themePick = document.createElement("div");
+  themePick.classList.add("theme-option");
+  themePick.getAttribute("data-color", color.color);
+  themePick.style.backgroundColor = color.valueSecondary;
+  themesPicks.push({ value: color.color, element: themePick });
+  themesContainer.appendChild(themePick);
+});
+
+let themeSelected = [COLORS[0].valuePrimary, COLORS[0].valueSecondary];
+themesPicks.forEach((theme) => {
+  theme.element.addEventListener("click", (e) => {
+    e.preventDefault();
+    const themePicked = COLORS.find((p) => p.color === theme.value);
+    themeSelected = [themePicked.valuePrimary, themePicked.valueSecondary];
+    setColors(theme.value);
+    console.log(themeSelected);
+  });
+  theme.element.addEventListener("mouseover", () => {
+    const colorShadow = COLORS.find((p) => p.color === theme.value);
+    theme.element.style.boxShadow = `0px 0px 4px ${colorShadow.valueSecondary}`;
+  });
+  theme.element.addEventListener("mouseout", () => {
+    theme.element.style.boxShadow = "none";
+  });
+});
+
+const theme = document.querySelector(".theme");
+const themeButton = document.querySelector(".theme-btn");
+
+themeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(theme.getAttribute("data-click"));
+  if (theme.getAttribute("data-click") === "false") {
+    theme.setAttribute("data-click", "true");
+    themeButton.classList.replace("bx-palette", "bx-x");
+    theme.style.right = "0px";
+  } else {
+    theme.setAttribute("data-click", "false");
+    themeButton.classList.replace("bx-x", "bx-palette");
+    theme.style.right = "-278px";
+  }
+});
+
+// TECLADO
+const keyboard = document.getElementById("keyboard");
+const keys = [
+  ["Q", "w", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
+  ["Z", "X", "C", "V", "B", "N", "M"],
+];
+keys.forEach((key) => {
+  const row = document.createElement("div");
+  row.classList.add("keyboard-row");
+  keyboard.appendChild(row);
+
+  key.forEach((letter) => {
+    row.innerHTML += `<button id="key-${letter}" class="key">${letter}</button>`;
+  });
+});
+
+const keyElements = [];
+for (let i = 0; i < keys.length; i++) {
+  for (let j = 0; j < keys[i].length; j++) {
+    const keyElement = document.getElementById(`key-${keys[i][j]}`);
+    keyElements.push({ element: keyElement, value: keys[i][j], used: false });
+  }
+}
+
+const drawLetter = (containerLetter, letter) => {
+  const letterContainer = document.createElement("div");
+  letterContainer.classList.add("letter");
+  letterContainer.textContent = letter;
+  containerLetter.appendChild(letterContainer);
+};
+
 // EFECTO CONFETTI
 const confettiWin = () => {
   const count = 200,
     defaults = {
       origin: { y: 0.7 },
-      colors: ["#ea5455", "#e84545", "#2d4059"],
+      colors: [...themeSelected, "#2d4059"],
     };
 
   function fire(particleRatio, opts) {
@@ -40,38 +177,6 @@ const confettiWin = () => {
     spread: 120,
     startVelocity: 45,
   });
-};
-
-// TECLADO
-const keyboard = document.getElementById("keyboard");
-const keys = [
-  ["Q", "w", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
-  ["Z", "X", "C", "V", "B", "N", "M"],
-];
-keys.forEach((key) => {
-  const row = document.createElement("div");
-  row.classList.add("keyboard-row");
-  keyboard.appendChild(row);
-
-  key.forEach((letter) => {
-    row.innerHTML += `<button id="key-${letter}" class="key">${letter}</button>`;
-  });
-});
-
-const keyElements = [];
-for (let i = 0; i < keys.length; i++) {
-  for (let j = 0; j < keys[i].length; j++) {
-    const keyElement = document.getElementById(`key-${keys[i][j]}`);
-    keyElements.push({ element: keyElement, value: keys[i][j], used: false });
-  }
-}
-
-const drawLetter = (containerLetter, letter) => {
-  const letterContainer = document.createElement("div");
-  letterContainer.classList.add("letter");
-  letterContainer.textContent = letter;
-  containerLetter.appendChild(letterContainer);
 };
 
 // MUESTRA LETRA POR LETRA
@@ -126,7 +231,7 @@ keyElements.forEach((key) => {
     if (!key.used) {
       key.used = true;
       if (word.includes(key.value)) {
-        key.element.style.backgroundColor = "#54ea8145";
+        key.element.style.backgroundColor = "#54ea8140";
         key.element.style.border = "1px solid #54ea81";
         containerWord.innerHTML = "";
         for (let i = 0; i < word.length; i++) {
@@ -134,8 +239,11 @@ keyElements.forEach((key) => {
           drawLetter(containerWord, lettersCorrects[i]);
         }
       } else {
-        key.element.style.backgroundColor = "#ea545545";
+        // key.element.style.backgroundColor = "##181e2740";
+        // // root.style.getPropertyValue("--secondary") + "40";
         key.element.style.border = "1px solid #ea5455";
+        // // "1px solid " + root.style.getPropertyValue("--secondary");
+        key.element.style.transform = "scale(0.000001)";
         lives--;
         livesCount.textContent = lives;
       }
